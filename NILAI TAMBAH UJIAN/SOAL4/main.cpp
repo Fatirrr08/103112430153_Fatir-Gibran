fa05fb46e1d4b8f1b92f0be7285c5e25decb273c
Fatirrr08 <fatingibran0802@gmail.com>
2025-11-10 18:44:45 +0700

    update

--- NILAI TAMBAH UJIAN/SOAL4/main.cpp
+++ NILAI TAMBAH UJIAN/SOAL4/main.cpp
@@ -6,7 +6,6 @@ int main() {
     std::cout << "PENGUJIAN SOAL 4: Queue Antrian Printer\n";
     std::cout << "========================================\n";
 
-    // Blok untuk tes skenario 1
     {
         std::cout << "--- Tes Skenario 1 (Kertas = 50) ---\n";
         PrinterQueue Q_sim1; // Constructor
@@ -20,10 +19,8 @@ int main() {
         Q_sim1.antriPrinter({"Laporan Bulanan", "Ayu", 5}); 
 
         Q_sim1.simulasiCetak(50);
-        // Destructor Q_sim1 dipanggil
     }
 
-    // Blok untuk tes skenario 2
     {
         std::cout << "\n--- Tes Skenario 2 (Kertas = 150) ---\n";
         PrinterQueue Q_sim2; // Constructor
@@ -34,7 +31,6 @@ int main() {
         Q_sim2.antriPrinter({"Presensi", "Nia", 24});
 
         Q_sim2.simulasiCetak(150);
-        // Destructor Q_sim2 dipanggil
     }
 
     return 0;
--- NILAI TAMBAH UJIAN/SOAL4/soal1.h
+++ NILAI TAMBAH UJIAN/SOAL4/soal1.h
@@ -1,14 +1,12 @@
 #pragma once
 #include <string>
 
-// Struct untuk data
 struct infotype_doc {
     std::string namaDok;
     std::string namaPengguna;
     int hal;
 };
 
-// Struct untuk node
 struct elmQ {
     infotype_doc info;
     elmQ* next;
@@ -16,7 +14,6 @@ struct elmQ {
 
 typedef elmQ* address_queue;
 
-// Class untuk Queue
 class PrinterQueue {
 public:
     PrinterQueue();  // Constructor
@@ -24,20 +21,15 @@ public:
 
     bool is_empty() const;
 
-    // Soal 4a: Prosedur enqueue
     void enqueue(address_queue p);
-    // Soal 4b: Prosedur dequeue (return bool)
     bool dequeue(address_queue &p);
     
-    // Soal 4c: Prosedur antriPrinter
     void antriPrinter(infotype_doc doc);
-    // Soal 4d: Prosedur simulasiCetak
     void simulasiCetak(int kertas);
 
 private:
     address_queue head;
     address_queue tail;
     
-    // Helper function dijadikan private
     address_queue createNewElm_queue(infotype_doc x);
 };
